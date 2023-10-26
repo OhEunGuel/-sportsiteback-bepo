6e84c55bd639cd2ef1e7b3f98e2c4a8b315f9288
han_desktop <dusrb003@naver.com>
2023-10-27 00:31:43 +0900

    10/27 bepo

--- src/main/java/com/example/sportsbatingproject/repository/article/entity/Article.java
+++ src/main/java/com/example/sportsbatingproject/repository/article/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.sportsbatingproject.repository.article.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,20 +12,29 @@ import java.util.Date;
 @Setter
 @Table(name = "news")
 public class Article  {
+
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+
   @Column(name = "title")
   private String title;
+
   @Column(name = "content")
   private String content;
+
   @Column(name = "url")
   private String url;
+
   @Column(name = "news_content")
   private String newsContent;
+
   @Column(name = "press")
   private String press;
+
   @Column(name = "news_time")
   private String newsTime;
+
   @Column(name = "image_url")
   private String imageUrl;
 
